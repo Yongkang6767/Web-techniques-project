9c1b9dbb0068f58c5ce3aa0a947d609f858b3360
Yongkang702 <Kanglim0702@gmail.com>
2026-07-05 08:57:50 +0800

    hi

---
+++ database/tourism_db.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS tourism_db;
+USE tourism_db;
+
+-- Create tables
+CREATE TABLE IF NOT EXISTS destinations (...);
+CREATE TABLE IF NOT EXISTS itineraries (...);
+
+-- Insert data (all 6 destinations)
+INSERT INTO destinations VALUES (...);
+INSERT INTO destinations VALUES (...);
+-- etc...
---
+++ database/tourism_db_structure.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE IF NOT EXISTS tourism_db;
+USE tourism_db;
+CREATE TABLE IF NOT EXISTS destinations (...);
+CREATE TABLE IF NOT EXISTS itineraries (...);
